08f629349d0dde440ac56e469b85bf6d177b33a8
Sachini Sahasra <98446017+Sahasra0108@users.noreply.github.com>
2024-04-01 14:32:21 +0530

    Create UserActivityLog Entity

--- src/main/java/CentralSync/demo/model/UserActivityLog.java
+++ src/main/java/CentralSync/demo/model/UserActivityLog.java
@@ -1,2 +1,55 @@
-package CentralSync.demo.model;public class UserActivityLog {
+package CentralSync.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class UserActivityLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String action;
+
+    private LocalDateTime timestamp;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
 }
