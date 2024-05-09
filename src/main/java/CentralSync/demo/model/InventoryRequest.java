6ad453ddeea72889ca8e78333ea45ea126738f51
MaleeshaVidurath <109060887+MaleeshaVidurath@users.noreply.github.com>
2024-05-09 20:19:47 +0530

    add lobok annotation to getter setter methods

--- src/main/java/CentralSync/demo/model/InventoryRequest.java
+++ src/main/java/CentralSync/demo/model/InventoryRequest.java
@@ -6,10 +6,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 
+@Setter
+@Getter
 @Entity
 public class InventoryRequest {
 
@@ -48,103 +52,6 @@ public class InventoryRequest {
 
     @Enumerated(EnumType.STRING)
     private ItemGroupEnum itemGroup;
-
-//reqId getter and setter
-    public long getReqId() {
-        return reqId;
-    }
-
-    public void setReqId(long reqId) {
-        this.reqId = reqId;
-    }
-//itemId getter and setter
-    public String getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
-//itemName getter and setter
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-//quantity getter and setter
-    public String getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(String quantity) {
-        this.quantity = quantity;
-    }
-//date getter and setter
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-//reason getter and setter
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-//description getter and setter
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-//employeeName getter and setter
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-//employeeID getter and setter
-    public long getEmployeeID() {
-        return employeeID;
-    }
-
-    public void setEmployeeID(long employeeID) {
-        this.employeeID = employeeID;
-    }
-//department getter and setter
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-//itemGroup getter and setter
-    public ItemGroupEnum getItemGroup() {
-        return itemGroup;
-    }
-
-    public void setItemGroup(ItemGroupEnum itemGroup) {
-        this.itemGroup = itemGroup;
-    }
-//reqStatus getter and setter
-    public StatusEnum getReqStatus() {
-        return reqStatus;
-    }
-
-    public void setReqStatus(StatusEnum reqStatus) {
-        this.reqStatus = reqStatus;
-    }
 
 }
 
