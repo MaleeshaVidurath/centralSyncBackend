3b238febcbd870cd153cd3693c6eb14b34936e95
Siluni <wijesinghe.anj@gmail.com>
2024-05-17 22:17:59 +0530

    date type mismatch corrected

--- src/main/java/CentralSync/demo/repository/StockInRepository.java
+++ src/main/java/CentralSync/demo/repository/StockInRepository.java
@@ -5,13 +5,14 @@ import CentralSync.demo.model.ItemGroupEnum;
 import CentralSync.demo.model.StockIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface StockInRepository extends JpaRepository<StockIn,Long> {
     List<StockIn> findAllByItemGroup(ItemGroupEnum itemGroup);
 
-    List<StockIn> findAllByDateBetween(Date startDate,Date endDate);
+    List<StockIn> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 
 
 }
--- src/main/java/CentralSync/demo/repository/StockOutRepository.java
+++ src/main/java/CentralSync/demo/repository/StockOutRepository.java
@@ -5,11 +5,12 @@ import CentralSync.demo.model.StockIn;
 import CentralSync.demo.model.StockOut;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface StockOutRepository extends JpaRepository<StockOut,Long> {
     List<StockOut> findAllByItemGroup(ItemGroupEnum itemGroup);
-    List<StockOut> findAllByDateBetween(Date startDate, Date endDate);
+    List<StockOut> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 }
 
