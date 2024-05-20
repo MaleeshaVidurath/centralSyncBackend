4d7db537b184e39255c39ab092ea96f6c72f8547
MaleeshaVidurath <109060887+MaleeshaVidurath@users.noreply.github.com>
2024-05-20 19:46:17 +0530

    commited

--- src/main/java/CentralSync/demo/model/User.java
+++ src/main/java/CentralSync/demo/model/User.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.*;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -35,9 +36,9 @@ public class User {
     private String email;
 
    // @Past(message = "Date of birth must be in the past")
-    @Past(message = "Date should be past")
-    @NotNull(message = "Date is required")
-    private Date dateOfBirth;
+    //@Past(message = "Date should be past")
+    //@NotNull(message = "Date is required")
+    private LocalDate dateOfBirth;
     @NotBlank(message = "Adress is required")
     private String address;
     @NotBlank(message = "Department is required")
@@ -102,11 +103,11 @@ public class User {
         this.email = email;
     }
 
-    public Date getDateOfBirth() {
+    public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(Date dateOfBirth) {
+    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
