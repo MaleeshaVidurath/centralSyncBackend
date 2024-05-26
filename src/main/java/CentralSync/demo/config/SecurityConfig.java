219c8eab3ce4b40a93066f9a12c1a704faf83e76
MaleeshaVidurath <109060887+MaleeshaVidurath@users.noreply.github.com>
2024-05-26 09:29:00 +0530

    commited

--- src/main/java/CentralSync/demo/config/SecurityConfig.java
+++ src/main/java/CentralSync/demo/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
