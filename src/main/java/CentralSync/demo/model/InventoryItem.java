86d5e9e352f903cf5dc1e4c9694b829977ffcde1
Siluni <wijesinghe.anj@gmail.com>
2024-06-19 13:18:02 +0530

    add image uploading and retrieving

--- src/main/java/CentralSync/demo/model/InventoryItem.java
+++ src/main/java/CentralSync/demo/model/InventoryItem.java
@@ -46,6 +46,7 @@ public class InventoryItem {
 
     @Enumerated(EnumType.STRING)
     private StatusEnum status;
+    private  String filePath;
 
     @OneToMany(mappedBy = "inventoryItem", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference("inventoryItem-inventoryRequests")
